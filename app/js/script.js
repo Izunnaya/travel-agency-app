d135558f6b8d130d828d4ab7cee4bbdbfededf8c
Michael-ugorji <izunaya11@gmail.com>
2022-06-26 01:19:12 +0100

    Worked on making navbar resposive on medium-sized devices also created a functional navbar button

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,8 @@
 const navItems = document.querySelector(".nav__items");
 const navLinks = document.querySelectorAll("#nav__links");
-// Open nav
 
 const navSlider = () => {
+  // Open nav
   const openNav = document.querySelector(".menuBtn");
   openNav.addEventListener("click", (e) => {
     console.log(e.target);
@@ -10,6 +10,7 @@ const navSlider = () => {
     openNav.classList.toggle("active");
   });
 
+  // This is the function that runs when each link is clicked.
   navLinks.forEach((link) => {
     link.addEventListener("click", () => {
       navItems.classList.remove("active");
